93d7ee832f42efed3d42cd6e6902419151f8b714
Jhon Roly Projects <92251336+jhonrolyol@users.noreply.github.com>
2022-10-20 19:31:15 -0500

    Commit

---
+++ 01_HelloWord/Module1.vb
@@ -0,0 +1,17 @@
+﻿' Link of the lectures: https://www.youtube.com/playlist?list=PLC601DEA22187BBF1
+
+Module Module1
+
+    Sub Main()
+        ' Preamble:
+        ' Note1.- Use "F5" for compile.
+        ' Note2.- Use "Control + S" for file save.
+
+        ' Main:
+        ' Main1.- Write "Hello World" by console.
+        Console.WriteLine("¡Hello World!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ 02_PrimitiveDataTypes/Module1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.SqlTypes
+
+Module Module1
+
+    Sub Main()
+        ' Preamble:
+        ' Note1.- Use "F5" for compile.
+        ' Note2.- Use "Control + S" for file save.
+
+        ' Main:
+        ' Main1.- Primitive Data Types 
+
+        ' 1.- Numbers
+
+        ' 1.1.- Integers
+        ' 1.1.1.- Byte
+        ' 1.1.2.- Short
+        ' 1.1.3.- Int
+        ' 1.1.4.- Long
+
+        ' 1.2.- Real Numbers
+        ' 1.2.1.- Double
+        ' 1.2.2.- Float
+
+
+        ' 2.- Characters
+        ' 2.1.- Char
+        ' 2.2.- String
+
+
+        ' 3.- Logical
+        ' 3.1.- Boolean
+
+
+    End Sub
+
+End Module
